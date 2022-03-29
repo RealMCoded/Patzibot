aa5955969c4c2b6408c8c004648803726329492a
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-03-29 10:37:46 -0400

    Update calculator.js

--- commands/calculator.js
+++ commands/calculator.js
@@ -1,4 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment} = require('discord.js');
+const path = require("path")
+const fs = require("fs")
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -38,7 +41,8 @@ module.exports = {
         if (interaction.options.getString('mode') == "divide"){
 
             if (interaction.options.getInteger('numerator') == 0 && interaction.options.getInteger('denominator') == 0) {
-                await interaction.reply(`🧮 **0 / 0 =** https://cdn.discordapp.com/attachments/808339703547428884/953407220882747412/unknown.png`);
+                //await interaction.reply(`🧮 **0 / 0 =** https://cdn.discordapp.com/attachments/808339703547428884/953407220882747412/unknown.png`);
+                await interaction.reply({ content:"🧮 **0 / 0 =**", files: [new MessageAttachment(fs.readFileSync(path.join(__dirname, `/resources/png/idk.png`)))] });
             } else {await interaction.reply(`🧮 **${interaction.options.getInteger('numerator')} / ${interaction.options.getInteger('denominator')} = ${interaction.options.getInteger('numerator') / interaction.options.getInteger('denominator')}**`);}
         }
 	},
