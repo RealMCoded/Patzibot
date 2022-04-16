3db6ff377defcc152af573d46a336c5969f8dd78
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-04-16 00:19:38 -0400

    /week command

---
+++ commands/week.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const week = require("./resources/json/weekvids.json")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('week')
+		.setDescription('Get a video for the current day of the week'),
+	async execute(interaction) {
+		const dayOfWeek = new Date().getDay();
+		const weekVid = week[dayOfWeek];
+		await interaction.reply(weekVid[Math.floor(Math.random() * (weekVid.length))])
+	},
+};
