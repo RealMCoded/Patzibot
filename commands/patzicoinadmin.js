f25446cd95262ca3a41b4d13a30b0fada5b52418
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-06-09 12:44:54 -0400

    Update patzicoinadmin.js

--- commands/patzicoinadmin.js
+++ commands/patzicoinadmin.js
@@ -8,6 +8,17 @@ module.exports = {
 		.setDescription(`patzercon admirn`)
 		.addSubcommand(subcommand => 
 			subcommand.setName("set")
+				.setDescription("set patzicoin to a user")
+				.addUserOption(option =>
+					option.setRequired(true)
+						.setName("user")
+						.setDescription("the user to give patzicoin to"))
+				.addIntegerOption(option => 
+					option.setRequired(true)
+						.setName("amount")
+						.setDescription("The amount of coins to give")))
+		.addSubcommand(subcommand => 
+			subcommand.setName("give")
 				.setDescription("give patzicoin to a user")
 				.addUserOption(option =>
 					option.setRequired(true)
@@ -52,7 +63,43 @@ module.exports = {
 			//logging
 			const embed = new MessageEmbed()
 				.setTitle("Patzicoin Logs")
-				.setDescription(`${user.tag}'s Patzicoins was set to **${amount}**\n\n(Executed by <@${interaction.user.id}>)`)
+				.setDescription(`${user.tag}'s Patzicoins was set to **${amount}**.\n\nExecutor: \`\`\`${interaction.user.id} | ${interaction.user.tag}\`\`\``)
+				.setColor("#00ff00")
+				.setTimestamp();
+			webhookClient.send({embeds: [embed]});
+
+		} else if (subcommand == "give") {
+			
+			const user = interaction.options.getUser('user');
+			const amount = interaction.options.getInteger('amount');
+			const userID = user.id;
+			const coins = await db.findOne({
+				where: {
+					userID: userID
+				}
+			});
+
+			if (coins == null) {
+				await db.create({
+					userID: userID,
+					coins: amount
+				});
+			} else {
+				await db.update({
+					coins: coins.coins + amount
+				}, {
+					where: {
+						userID: userID
+					}
+				});
+			}
+
+			interaction.reply({ content: `✅ **Gave ${user.username} ${amount} Patzicoins!**`, ephemeral: true })
+
+			//logging
+			const embed = new MessageEmbed()
+				.setTitle("Patzicoin Logs")
+				.setDescription(`${user.tag} was given **${amount}** Patzicoins\n\nExecutor: \`\`\`${interaction.user.id} | ${interaction.user.tag}\`\`\``)
 				.setColor("#00ff00")
 				.setTimestamp();
 			webhookClient.send({embeds: [embed]});
