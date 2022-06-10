6e67732df08f1d236023a435209dca7fd08845d3
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-06-09 21:25:33 -0400

    better impersonate command

--- commands/impersonate.js
+++ commands/impersonate.js
@@ -12,8 +12,22 @@ module.exports = {
 		.addStringOption(string =>
 			string.setName("string")
 				.setRequired(true)
-				.setDescription("The text to say.")),
+				.setDescription("The text to say."))
+		.addIntegerOption(option =>
+				option.setName('inmind')
+					.setDescription('in mind (Default: false)')
+					.setRequired(false)
+					.addChoice('True', 1)
+					.addChoice('False', 0)),
 	async execute(interaction) {
-		await interaction.reply(`\\*in ${interaction.options.getUser('user').username} voice\\* ${interaction.options.getString('string')}`);
+		const user = interaction.options.getUser('user').username
+		const string = interaction.options.getString('string');
+		const inMind = interaction.options.getInteger('inmind') || 0;
+
+		if(inMind == 1) {
+			await interaction.reply(`${user} (in mind): ${string}`);
+		} else {
+			await interaction.reply(`${user}: ${string}`);
+		}
 	},
 };
