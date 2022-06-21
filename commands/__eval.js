8b98e0bd2deb3d05ebee79287835971bad631c75
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-06-21 18:56:32 -0400

    Create __eval.js

---
+++ commands/__eval.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('eval')
+		.setDescription(`run js code`)
+		.addStringOption(string =>
+			string.setName("eval")
+				.setRequired(true)
+				.setDescription("eval"))
+		.addIntegerOption(option => 
+				option.setRequired(true)
+					.setName("showResult")
+					.setDescription("is slef")
+					.addChoice(`Show`, 1)
+					.addChoice(`Hide`, 0)),
+	async execute(interaction) {
+		if (interaction.user.id == 284804878604435476) {
+			const evl = eval(interaction.options.getString('eval'));
+			if (interaction.options.getInteger('showResult') == 1) {
+				await interaction.reply({ content: `\`\`\`eval\`\`\``, ephemeral: false });
+			} else {
+				await interaction.reply({ content: `\`\`\`eval\`\`\``, ephemeral: true });
+			}
+		} else {
+			await interaction.reply({ content: "❌ **You cannot use this command!**", ephemeral: true });
+		}
+	},
+};
