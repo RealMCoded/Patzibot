02320de816523f5de43e7677503342d9dcad9e8a
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-07-21 14:38:00 -0400

    Global commands: soon

---
+++ register-global.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
+const {token, clientId} = require('./config.json');
+
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
+
+const rest = new REST({version:9}).setToken(token);
+
+    (async () => {
+         try {
+            console.log('Registering commmands...');
+            await rest.put(
+                Routes.applicationCommands(clientId),
+                {body:commands},
+                );
+
+                console.log('Done!');
+            } catch (error) {
+                console.error(error);
+            }
+        })();
