7bf7064831f6795ec0a8da089a4981ebacec07af
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-07-28 18:52:59 -0400

    everyone can use /gasleak now

--- commands/gasleak.js
+++ commands/gasleak.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const perm = require('./powerList.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,11 +9,7 @@ module.exports = {
 				.setRequired(true)
 				.setDescription("love is in the air?")),
 	async execute(interaction) {
-		if (perm.includes(interaction.user.id)) {
 			let str = interaction.options.getString('string')
 			await interaction.reply({ content: "*note: this may look a little* ***funky*** *on mobile.*```ansi\n" + str + "?\n\nWRONG! [2;40m  [2;37mgas leak[0m[2;40m    [0m[2;40m \n                      [0m[2;40m[0m```", ephemeral: false });
-		} else {
-			await interaction.reply({ content: "❌ **You cannot use this command!**", ephemeral: true });
-		}
 	},
 };
