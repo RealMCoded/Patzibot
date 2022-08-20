92f056f3858ac9432deff5544fea97c23408e9c5
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-08-20 11:46:40 -0400

    Update jokeban.js

--- commands/jokeban.js
+++ commands/jokeban.js
@@ -3,26 +3,27 @@ const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('jokeban')
-		.setDescription(`Ban a user [JOKE COMMAND]`)
-		.addUserOption(user =>
-			user.setName("user")
-				.setRequired(true)
-				.setDescription("The member to ban"))
-		.addStringOption(reason =>
-			reason.setName("reason")
-				//.setRequired(true)
-				.setDescription("The reason for the ban")),
+		.setName('ban')
+		.setDescription(`fake ban command`)
+		.addSubcommand(subcommand =>
+			subcommand
+			.setName("joke")
+			.setDescription(`Ban someone, but not really!`)
+			.addUserOption(user =>
+				user.setName("user")
+					.setRequired(true)
+					.setDescription("The member to \"ban\""))
+			.addStringOption(reason =>
+				reason.setName("reason")
+					.setDescription("The reason for the \"ban\""))),
 	async execute(interaction) {
-		//await interaction.reply(`<a:typing:944765274475864094>  **Getting ${interaction.options.getUser('user')}'s IP Address** `);
-
 		var reason = interaction.options.getString('reason')
 
 		if (!reason) reason = "No reason specified."
 
 		await interaction.deferReply();
-		await wait(2000);
-		await interaction.editReply(`✅ Banned **${interaction.options.getUser('user')}** for "**${reason}**"\n\n*haha jk get TROLLED!*`);
+		await wait(1500);
+		await interaction.editReply(`✅ Banned **${interaction.options.getUser('user')}** for "**${reason}**"\n\n||jk get trolled <:trollface:909953790977900595>||`);
 	},
 };
 
