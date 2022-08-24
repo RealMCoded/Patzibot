9dcf6ed05046ac4be75f5fdf4c4f391628ca6567
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-08-24 11:25:12 -0400

    Patzicoin Search command, Changed some wording

--- commands/patzicoin.js
+++ commands/patzicoin.js
@@ -58,14 +58,23 @@ module.exports = {
 					subcommand.setName("beg")
 						.setDescription("beg craig for patzicoins"))
 		.addSubcommand(subcommand => 
+			subcommand.setName("search")
+				.setDescription("Search for Patzicoins!")
+				.addStringOption(option =>
+					option.setName('location')
+						.setDescription('the place to look')
+						.setRequired(true)
+						.addChoice('McDonalds Dumpster (Pay: 5-15 | 3% lose rate)', `1`)
+						.addChoice('Break into a car (Pay: 50-100 | 50% lose rate)', `2`)))
+		.addSubcommand(subcommand => 
 			subcommand.setName("work")
 				.setDescription("Work to earn Patzicoins!")
 				.addStringOption(option =>
 					option.setName('job')
 						.setDescription('the job')
 						.setRequired(true)
-						.addChoice('McDonalds (Pay: 15-30 | 5% lose rate)', `1`)
-						.addChoice('Dollar Store (Pay: 35-50 | 10% lose rate)', `2`))),
+						.addChoice('McDonalds (Pay: 15-34 | 5% lose rate)', `1`)
+						.addChoice('Corner Store (Pay: 35-50 | 10% lose rate)', `2`))),
 	async execute(interaction) {
 		const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 		const subcommand = interaction.options.getSubcommand();
@@ -74,7 +83,7 @@ module.exports = {
 		if (subcommand == "about") {
 			const embed = new MessageEmbed()
 				.setTitle("About PatziCoins")
-				.setDescription("PatziCoins are a currency used in the Patzi's World Discord server. They can be used to buy special items and rewards.\n\nYou can earn PatziCoins by chatting in the server, and you can spend them on special items and rewards.\n\nNote: The PatziCoin system is currently in Beta and may change at any time.")
+				.setDescription("PatziCoins are a currency used in the Patzi's World Discord server. They can be used to buy special items and rewards and to show off to other server members how active you are.\n\nYou can earn PatziCoins by chatting in the server or by working!\n\nNote: The PatziCoin system is currently a work in progress and may change at any time.")
 			interaction.reply({embeds: [embed]});
 		} else if(subcommand == "bank") {
 			const usr = interaction.user
@@ -115,8 +124,11 @@ module.exports = {
 			} else if (mode == 2) {
 				if(amount == 0) amount = tag.bank
 				if (amount > tag.bank) {
-					interaction.reply("You don't have that many PatziCoins in your bank!");
-					return;
+					const embed = new MessageEmbed()
+						.setTitle("Withdraw PatziCoins")
+						.setDescription(`You cannot withdraw negative PatziCoins!`)
+						.setColor("#FF0000")
+					return interaction.reply({embeds: [embed]});
 				} else {
 					tag.update({
 						coins: tag.coins + amount,
@@ -164,7 +176,7 @@ module.exports = {
 			if (chance < 55) { //55% chance of success
 				const embed = new MessageEmbed()
 					.setTitle("Robbery")
-					.setDescription(`You tried to rob **${target.username}**, but they were too quick and they ran away!`)
+					.setDescription(`You tried to rob **${target.username}**, but they were too quick and ran away!`)
 					.setColor("#ff0000")
 				interaction.reply({embeds: [embed]});
 				recentrobbers.add(usr.id);
@@ -222,7 +234,6 @@ module.exports = {
 
 					db.increment('coins', { by: amount, where: { userID: interaction.user.id } });
 				}
-
 			}
 			if (method == "2") {
 
@@ -240,6 +251,63 @@ module.exports = {
 						.setTitle("Work at Dollar Store - PatziCoin")
 						.setColor(0x00FF00)
 						.setDescription(`"${msg}" - boss\nYou earned **${amount}** PatziCoins!`)
+					interaction.reply({embeds: [embed]});
+
+					db.increment('coins', { by: amount, where: { userID: interaction.user.id } });
+				}
+
+			}
+
+			recent.add(interaction.user.id );
+            setTimeout(() => {
+                // Removes the user from the set after 60 seconds
+                recent.delete(interaction.user.id );
+                }, 35000);
+			}
+		} else if(subcommand == "search"){
+			if(recent.has(interaction.user.id)){
+				interaction.reply({content:`⏰ **You cannot search for PatziCoins right now!**\n**You have to wait a while to use it again!**`,ephemeral: true});
+				return;
+			} else {
+			var method = interaction.options.getString("location");
+
+			var dbusr = await db.findOrCreate({
+				where: { userID: interaction.user.id },
+			});
+
+			if (method == "1") {
+
+				if(Math.random() < 0.05){
+					const embed = new MessageEmbed()
+						.setTitle("Search for PatziCoins at the McDonalds Dumpster - PatziCoin")
+						.setColor(0xFF0000)
+						.setDescription(`The dumpster was empty. You found nothing.`)
+					interaction.reply({embeds: [embed]});
+				} else {
+					var amount = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+					const embed = new MessageEmbed()
+						.setTitle("Search for PatziCoins at the McDonalds Dumpster  - PatziCoin")
+						.setColor(0x00FF00)
+						.setDescription(`You found **${amount}** PatziCoins!`)
+					interaction.reply({embeds: [embed]});
+
+					db.increment('coins', { by: amount, where: { userID: interaction.user.id } });
+				}
+			}
+			if (method == "2") {
+
+				if(Math.random() < 0.10){
+					const embed = new MessageEmbed()
+						.setTitle("Break into a car - PatziCoin")
+						.setColor(0xFF0000)
+						.setDescription(`You were caught and sent to jail.\nYou lost **0** PatziCoins.`)
+					interaction.reply({embeds: [embed]});
+				} else {
+					var amount = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+					const embed = new MessageEmbed()
+						.setTitle("Break into a car - PatziCoin")
+						.setColor(0x00FF00)
+						.setDescription(`Somehow the alarms didn't go off in the car! You found **${amount}** PatziCoins!`)
 					interaction.reply({embeds: [embed]});
 
 					db.increment('coins', { by: amount, where: { userID: interaction.user.id } });
@@ -327,68 +395,68 @@ module.exports = {
 			return interaction.reply({embeds: [embed]});
 		}
 
-	} else if(subcommand == "beg"){
-		let delay = 18000 //~5 hours
-		var dbusr = await db.findOrCreate({
-			where: { userID: interaction.user.id },
-		});
+		} else if(subcommand == "beg"){
+			let delay = 18000 //~5 hours
+			var dbusr = await db.findOrCreate({
+				where: { userID: interaction.user.id },
+			});
 
-		const tag = await db.findOne({ where: { userID: interaction.user.id } });
-		const lastBeg= parseInt(tag.get('lastBegClaimDate'))
-		//console.log(`${lastBeg+43200}, now is ${Date.now()}`)
+			const tag = await db.findOne({ where: { userID: interaction.user.id } });
+			const lastBeg= parseInt(tag.get('lastBegClaimDate'))
+			//console.log(`${lastBeg+43200}, now is ${Date.now()}`)
 
-		//get current data as unix MINUS MILLISECONDS
-		var n = Math.floor(Date.now() / 1000)
+			//get current data as unix MINUS MILLISECONDS
+			var n = Math.floor(Date.now() / 1000)
 
-		if(n <= lastBeg+delay){
-			//get time left between now and last beg
-			const embed = new MessageEmbed()
-				.setTitle("Beg - PatziCoin")
-				.setColor("#FF0000")
-				.setDescription(`"Go away! You can beg again <t:${lastBeg+delay}:R> (<t:${lastBeg+delay}:f>)"\n -craig`)
-				.setTimestamp()
-			return interaction.reply({embeds: [embed]});
-		} else {
-			let amount = Math.floor(Math.random() * (125 - 1 + 1)) + 1;
-
-			db.increment('coins', { by: amount, where: { userID: interaction.user.id } });
-			db.update({ lastBegClaimDate: n }, { where: { userID: interaction.user.id } });
-			if(amount == 69){
-				db.increment('coins', { by: 1, where: { userID: interaction.user.id } });
+			if(n <= lastBeg+delay){
+				//get time left between now and last beg
 				const embed = new MessageEmbed()
-				.setTitle("Beg - PatziCoin")
-				.setColor("#00FF00")
-				.setDescription(`"Here's **${amount} PatziCoins**, now go awa-\n\nWhat's this on the floor? Another Patzicoin? Here, you can have it! That brings your total to **${amount+1}**. Now leave!"\n -craig`)
-				.setTimestamp()
-			return interaction.reply({embeds: [embed]});
+					.setTitle("Beg - PatziCoin")
+					.setColor("#FF0000")
+					.setDescription(`"Go away! You can beg again <t:${lastBeg+delay}:R> (<t:${lastBeg+delay}:f>)"\n -craig`)
+					.setTimestamp()
+				return interaction.reply({embeds: [embed]});
 			} else {
+				let amount = Math.floor(Math.random() * (125 - 1 + 1)) + 1;
+
+				db.increment('coins', { by: amount, where: { userID: interaction.user.id } });
+				db.update({ lastBegClaimDate: n }, { where: { userID: interaction.user.id } });
+				if(amount == 69){
+					db.increment('coins', { by: 1, where: { userID: interaction.user.id } });
+					const embed = new MessageEmbed()
+					.setTitle("Beg - PatziCoin")
+					.setColor("#00FF00")
+					.setDescription(`"Here's **${amount} PatziCoins**, now go awa-\n\nWhat's this on the floor? Another Patzicoin? Here, you can have it! That brings your total to **${amount+1}**. Now leave!"\n -craig`)
+					.setTimestamp()
+				return interaction.reply({embeds: [embed]});
+				} else {
+				const embed = new MessageEmbed()
+					.setTitle("Beg - PatziCoin")
+					.setColor("#00FF00")
+					.setDescription(`"Here's **${amount} PatziCoins**, now go away!"\n -craig`)
+					.setTimestamp()
+				return interaction.reply({embeds: [embed]});
+				}
+			}
+		} else if(subcommand == "serverstats"){
+			var le = 0
+			var le2 = 0
+
+			list = await db.findAll({
+				attributes: ['coins', 'bank']
+			})
+
+			for(var i=0; i < list.length; i++){
+				le+=list[i].coins
+				le2+=list[i].bank
+			}
+
 			const embed = new MessageEmbed()
-				.setTitle("Beg - PatziCoin")
-				.setColor("#00FF00")
-				.setDescription(`"Here's **${amount} PatziCoins**, now go away!"\n -craig`)
+				.setTitle("Server Stats - PatziCoin")
+				.setColor("#0099ff")
+				.setDescription(`**Total Users with PatziCoins**: ${list.length}\n**Total PatziCoins**: ${le} 🪙 (avg. ${Math.round((le/list.length)*100)/100} per member)\n**Total Bank**: ${le2}`)
 				.setTimestamp()
 			return interaction.reply({embeds: [embed]});
-			}
 		}
-	} else if(subcommand == "serverstats"){
-		var le = 0
-		var le2 = 0
-
-		list = await db.findAll({
-			attributes: ['coins', 'bank']
-		})
-
-		for(var i=0; i < list.length; i++){
-			le+=list[i].coins
-			le2+=list[i].bank
-		}
-
-		const embed = new MessageEmbed()
-			.setTitle("Server Stats - PatziCoin")
-			.setColor("#0099ff")
-			.setDescription(`**Total Users**: ${list.length}\n**Total PatziCoins**: ${le} 🪙 (avg. ${Math.round((le/list.length)*100)/100} per member)\n**Total Bank**: ${le2}`)
-			.setTimestamp()
-		return interaction.reply({embeds: [embed]});
-	}
 	},
 };
