f8de01466c5a16bd0a6008d164951f75c3260fb3
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-08-26 18:53:49 -0400

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -4,6 +4,40 @@ const project = require('../package.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
+        .setName('patzibot')
+        .setDescription('intro related commands')
+        .addSubcommand(subcommand =>
+            subcommand
+            .setName("about")
+            .setDescription("Learn more about me!"))
+        .addSubcommand(subcommand =>
+            subcommand
+            .setName("version")
+            .setDescription("What version am i on? I don't know!")),
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+
+        if (subcommand == "version"){
+            revision = require('child_process')
+                .execSync('git rev-parse HEAD')
+                .toString().substring(0, 7);
+            const currentTimestamp = Math.round(+new Date() / 1000);
+            const timestampColour = currentTimestamp.toString(16).substring(2, 10);
+            const embed = new MessageEmbed()
+                //.setThumbnail("https://cdn.discordapp.com/avatars/876729461188464660/43fb19cac4985b40677f47e1d8b476d4.png?size=4096")
+                .setTitle('PatziBot')
+                .setDescription(`Version ${project.version} (commit \`${revision}\`)\n\nBot created by <@284804878604435476>\n\nIcon created by <@518567024545497113>\n\n\`/kill\`, \`/quiz\` command created by Zai#1113`)
+                .setColor(`${timestampColour}`);
+            await interaction.reply({embeds: [embed]});
+        } else {
+            interaction.reply({content:`⚠ **The intro is not ready yet! Come back later.**`,ephemeral: true});
+        }
+    },
+};
+
+/*
+module.exports = {
+    data: new SlashCommandBuilder()
         .setName('about')
         .setDescription('who am i'),
     async execute(interaction) {
@@ -19,4 +53,4 @@ module.exports = {
             .setColor(`${timestampColour}`);
         await interaction.reply({embeds: [embed]});
     },
-};
+};*/
