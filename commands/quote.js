a5686898d5c74c500bfb0ac989526dfd1848723b
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-11-11 17:44:58 -0500

    Update quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -4,7 +4,7 @@ const quotes = require("./resources/json/quotes.json")
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('quote')
-		.setDescription('random quote lol - stolen from @PatziQuotes'),
+		.setDescription('Quotes stolen from @PatziQuotes'),
 	async execute(interaction) {
 		await interaction.reply(quotes[Math.floor(Math.random() * (quotes.length))])
 	},
