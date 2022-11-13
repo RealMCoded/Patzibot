3fbdcd3de711b9f98cfc6c01441a961783189de3
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-11-13 15:33:03 -0500

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -1,21 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('dice')
-		.setDescription('Roll a die!')
+		.setDescription('Roll a dice!')
 		.addIntegerOption(sides =>
             sides.setName("sides")
-                .setDescription("The sides of the die (default: 6)")),
+                .setDescription("How many sides the dice has (default: 6)")),
 	async execute(interaction) {
+		var sides = interaction.options.getInteger('sides') || 6
 
-		var sides = interaction.options.getInteger('sides')
-        if (!sides) {
-            sides = 6
-        } else {
-            interaction.options.getInteger('sides')
-        }
-
-		await interaction.reply(`The die 🎲 landed on **${Math.floor(Math.random() * sides)}**`);
+		//await interaction.reply(`The die 🎲 landed on **${Math.floor(Math.random() * sides)}**`);
+		const embed = new MessageEmbed()
+            .setTitle(`The dice landed on ${Math.floor(Math.random() * sides)}!`)
+			.setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/game-die_1f3b2.png")
+            .setColor('#FFFFFF');
+        await interaction.reply({embeds: [embed]});
 	},
 };
