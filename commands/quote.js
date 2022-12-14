4d318055c25cb8cd1fd3978b196fcdb52adb22a4
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-12-13 21:32:45 -0500

    Quote IDs

--- commands/quote.js
+++ commands/quote.js
@@ -4,8 +4,18 @@ const quotes = require("./resources/json/quotes.json")
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('quote')
-		.setDescription('Quotes stolen from @PatziQuotes'),
+		.setDescription('Quotes stolen from @PatziQuotes')
+		.addIntegerOption(option => 
+			option.setRequired(false)
+				.setName("quote-id")
+				.setDescription("The ID of the quote")),
 	async execute(interaction) {
-		await interaction.reply(quotes[Math.floor(Math.random() * (quotes.length))])
+		let quoteID;
+
+		if (interaction.options.getInteger('quote-id') < -1) {interaction.reply({ content: "⚠️ **ID must be greater than -1!**", ephemeral: true }); return;}
+		if (interaction.options.getInteger('quote-id') > quotes.length) {interaction.reply({ content: `⚠️ **ID must be less than ${quotes.length}!**`, ephemeral: true }); return;}
+
+		quoteID = interaction.options.getInteger('quote-id') || Math.floor(Math.random() * (quotes.length)) 
+		await interaction.reply(`Quote ID: \`${quoteID}\`\n${quotes[quoteID]}`)
 	},
 };
