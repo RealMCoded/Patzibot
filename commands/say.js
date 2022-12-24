2a8c95723e92b387fd85f0faa7aa4a494a8c8160
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-12-23 23:58:56 -0500

    /say replies

--- commands/say.js
+++ commands/say.js
@@ -8,11 +8,24 @@ module.exports = {
 		.addStringOption(string =>
 			string.setName("string")
 				.setRequired(true)
-				.setDescription("The text to say.")),
+				.setDescription("The text to say."))
+		.addStringOption(string =>
+			string.setName("replyMessageID")
+				.setRequired(false)
+				.setDescription("The Message ID to reply to.")),
 	async execute(interaction) {
 		if (powerList.includes(interaction.user.id)) {
-			await interaction.channel.send(interaction.options.getString('string'))
-			await console.log(`${interaction.user.tag} made the bot say "${interaction.options.getString('string')}"\n`)
+			let message, ping, replyID;
+			message = interaction.options.getString('string')
+			//ping = interaction.options.getBoolean('mention') || false
+			replyID = interaction.options.getString('replyMessageID') || "DO-NOT"
+
+			if (replyID == "DO-NOT") {
+				await interaction.channel.send(message)
+			} else {
+				await interaction.channel.send({ content: message, reply: { messageReference: replyId }});
+			}
+			await console.log(`${interaction.user.tag} made the bot say "${message}"\n`)
 			await interaction.reply({ content: "ok i said the thing u wanted me to say :)", ephemeral: true });
 		} else {
 			await interaction.reply({ content: "❌ **You cannot use this command!**", ephemeral: true });
