70299b4d874084464b705747f662a6d4ce884abc
stuartt <34777184+RealMCoded@users.noreply.github.com>
2024-06-28 23:03:48 -0400

    improved ask command

---
+++ commands/fun/ask.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+const { formatUsername } = require("../../util.js")
+const responses = require("../../resources/json/ask_responses.json")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ask')
+		.setDescription(`Ask PatziBot a question!`)
+		.addStringOption(question =>
+			question.setName("question")
+				.setRequired(true)
+				.setDescription("The question to ask. Under 1500 characters allowed")),
+	async execute(interaction) {
+		const question = interaction.options.getString('question')
+		if (question.length > 1500) {await interaction.reply({content: 'Your question is over **1500** characters!', ephemeral: true}); return;}
+		if (question.toUpperCase().includes("HOMOPHOBIC") || question.toUpperCase().includes("TRANSPHOBIC") || question.toUpperCase().includes("FAGGOT")){
+			await interaction.reply({content: 'patzibot has chosen not to answer this.', ephemeral: true})
+		} else if (question == "" || question =="‍" || question == "​" || question == "ㅤ" || question == "⠀" || question == "_ _" || question == "** **" || question == "*** ***"){
+			await interaction.reply({content: `**Tip of the day:** try actually typing something`, ephemeral: true});
+		} else {
+			await interaction.deferReply();
+			await wait(1000);
+			await interaction.editReply(`**${formatUsername(interaction.user)}**: "${interaction.options.getString('question')}"\n\n**PatziBot**: "${responses[Math.floor(Math.random() * responses.length)]}"`);
+		}
+	},
+};
