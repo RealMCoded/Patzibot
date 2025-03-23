b74d71e1eecbf2d086af7963ecb19fa71cc66ecf
stuartt <34777184+RealMCoded@users.noreply.github.com>
2025-03-23 01:32:33 -0400

    Update join and leave messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,8 +8,9 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setTitle(`${member.user.username} has joined ${member.guild.name}!`)
-            .setDescription(`Welcome to ${member.guild.name}, <@${member.user.id}>!\nWe now have \`${member.guild.memberCount}\` members.`)
+            .setDescription(`Hi hi hi **${member.user.username}**, Welcome to **${member.guild.name}**!\nWe now have \`${member.guild.memberCount}\` members.`)
             .setThumbnail(member.user.displayAvatarURL())
+            .setImage("https://cdn.discordapp.com/attachments/876731478980050994/1353232137897119796/image.png?ex=67e0e723&is=67df95a3&hm=7adb370adacff0b6703bc827e0a073be172c275ccc92f63db42ed7049c92d9c3&")
             .setColor(0x57F287);
 
         wh.send({
